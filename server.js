2d268a2f14426560d207010583dcaf9898c4ac49
Nicholas Nolen <nicnolen@ymail.com>
2022-03-09 15:29:13 -0600

    set up initial express server and connected MongoDB inside the server.js file using Mongoose

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+/* SERVER */
+// Import dependencies
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(require('./routes'));
+
+// Tell Mongoose what database we want to connect to
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/socially-social'
+);
+
+// Log mongo queries being executed
+mongoose.set('debug', true);
+
+// Connect to the correct port
+app.listen(PORT, () => console.info(`🌍 Connected on localhost:${PORT}`));
