d6b69829965773607257033ce92d4f67dbc09648
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 09:01:50 -0600

    added a reaction count virtual to retrieve the length of the thought's reactions array

--- models/Thought.js
+++ models/Thought.js
@@ -2,21 +2,34 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    maxLength: 280,
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal =>
+        moment(createdAtVal).format('MMMM Do YYYY [at] h:mm a'), // Formatted as Month Day Year at hour:minute am/pm
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal =>
-      moment(createdAtVal).format('MMMM Do YYYY [at] h:mm a'), // Formatted as Month Day Year at hour:minute am/pm
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema]
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
 });
