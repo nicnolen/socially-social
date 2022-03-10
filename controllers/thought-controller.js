76de9eb32a8aa2af4baf149f1769504395ee55da
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 13:25:18 -0600

    added functionality to find all thoughts and populated it with reactions in the thought-controller.js file

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,24 @@
+// Import dependencies
+const { User, Thought } = require('../models');
 
+// Create functionality for the Thought model
+const thoughtController = {
+  // GET all thoughts (callback function for `GET /api/thoughts`)
+  getAllThought(req, res) {
+    Thought.find({})
+      .populate({
+        path: 'reactions',
+        select: '-__v',
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.error(err);
+        res.sendStatus(400);
+      });
+  },
+};
+
+// Export the module
+module.exports = thoughtController;
