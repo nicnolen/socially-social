a11a0fe29f2f06a947206b7d39512b36c6f79306
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 11:33:28 -0600

    added functionality to find a user by id in the user-controller.js file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,7 +3,7 @@ const { User, Thought } = require('../models');
 
 // Create functionality for the User model
 const userController = {
-  // /api/users (GET all users)
+  // GET all users (callback function for `GET /api/users`)
   getAllUser(req, res) {
     User.find({})
       .select('-__v') // tell Mongoose we dont want to use the pizza's `__v` field
@@ -12,6 +12,25 @@ const userController = {
       .catch(err => {
         console.error(err);
         res.sendStatus(400);
+      });
+  },
+
+  // get one user by id (callback function for `GET /api/users/:id)
+  // destructured `params` out since thats the only data we need to fulfill the request
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .select('-__v')
+      .then(dbUserData => {
+        // if no user is found
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(400).json(err);
       });
   },
 };
