310e6b81c029a291cb02999c80d1c515968cdefb
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 12:46:37 -0600

    added functionality to add a new friend to a user's friend list

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -79,6 +79,23 @@ const userController = {
       })
       .catch(err => res.json(err));
   },
+
+  // add a new friend (callback function for `POST /api/users/:userid/fiends/:friendId`
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 };
 
 // Export the module
