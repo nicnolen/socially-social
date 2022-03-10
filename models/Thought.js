d28c834aafae49e61c6a9947300ecee7212cddd9
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 09:20:13 -0600

    added Reply schema to the Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -2,6 +2,37 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+// Create Reaction schema
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId, // tell Mongoose to expect an ObjectId
+      default: () => new Types.ObjectId(), // default value is a new ObjectId
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal =>
+        moment(createdAtVal).format('MMMM Do YYYY [at] h:mm a'),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// Create Thought schema
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -37,4 +68,4 @@ ThoughtSchema.virtual('reactionCount').get(function () {
 const Thought = model('Thought', ThoughtSchema);
 
 // Export module
-module.exports = Thought
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -18,7 +18,7 @@ const UserSchema = new Schema(
     },
     thoughts: [
       {
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId, // tell Mongoose to expect an object id
         ref: 'Thought',
       },
     ],
