31ead2651b23f699e131b8ee98d820cf3c31b791
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 13:49:18 -0600

    added route to add a new thought in thought-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,10 +3,11 @@ const router = require('express').Router;
 const {
   getAllThought,
   getThoughtById,
+  createThought,
 } = require('../../controllers/thought-controller');
 
 // Set up GET all and POST at /api/thoughts
-router.route('/').get(getAllThought);
+router.route('/').get(getAllThought).post(createThought);
 
 // Set up GET one, PUT, DELETE at /api/thoughts/:id
 router.route('/:id').get(getThoughtById);
