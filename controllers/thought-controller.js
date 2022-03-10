b582b867d95b14d9254e061603d8443287a2a4b4
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 13:48:34 -0600

    added functionality to add a thought in thought-controllers.js file

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -41,6 +41,26 @@ const thoughtController = {
         res.statusSend(400);
       });
   },
+
+  // add a thought to the database (callback function for `POST /api/thoughts)
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  },
 };
 
 // Export the module
