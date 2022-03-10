318ace6e08c2e61d326374d4b02bf7806a1e43d6
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 14:36:22 -0600

    added route to delete thought in thought-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,13 +4,14 @@ const {
   getAllThought,
   getThoughtById,
   createThought,
+  deleteThought,
 } = require('../../controllers/thought-controller');
 
 // Set up GET all and POST at /api/thoughts
 router.route('/').get(getAllThought).post(createThought);
 
 // Set up GET one, PUT, DELETE at /api/thoughts/:id
-router.route('/:id').get(getThoughtById).put(getThoughtById);
+router.route('/:id').get(getThoughtById).put(getThoughtById).delete(deleteThought);
 
 // Export the module
 module.exports = router;
