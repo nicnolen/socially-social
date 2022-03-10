ab46f7e36cbcaa36d9a985dd225d066d7e4a419d
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 12:21:09 -0600

    added DELETE route to delete users in the user-route.js folder

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,14 +4,15 @@ const {
   getAllUser,
   getUserById,
   createUser,
-  updateUser
+  updateUser,
+  deleteUser
 } = require('../../controllers/user-controller');
 
 // Set up GET all at /api/users
 router.route('/').get(getAllUser).post(createUser);
 
 // Set up GET one at /api/users/:id
-router.route('/:id').get(getUserById).put(updateUser);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 // Export the router
 module.exports = router;
