5e937b613fe6256eccf108428545a093228721b9
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 11:53:26 -0600

    added functionality to update a user by id in the user-controllers.js file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,6 +19,14 @@ const userController = {
   // destructured `params` out since thats the only data we need to fulfill the request
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
       .select('-__v')
       .then(dbUserData => {
         // if no user is found
@@ -38,6 +46,22 @@ const userController = {
   createUser({ body }, res) {
     User.create(body)
       .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // update user by id (callback function for `PUT /api/user/:id`)
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch(err => res.status(400).json(err));
   },
 };
