7559b7dfc43a2ee449d6d31b2a56b5a751a1b49e
Nicholas Nolen <nicnolen@ymail.com>
2022-03-10 13:42:50 -0600

    added route to find a thought by id in thought-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,9 +1,15 @@
 // Import dependencies
 const router = require('express').Router;
-const { getAllThought } = require('../../controllers/thought-controller');
+const {
+  getAllThought,
+  getThoughtById,
+} = require('../../controllers/thought-controller');
 
 // Set up GET all and POST at /api/thoughts
 router.route('/').get(getAllThought);
+
+// Set up GET one, PUT, DELETE at /api/thoughts/:id
+router.route('/:id').get(getThoughtById);
 
 // Export the module
 module.exports = router;
